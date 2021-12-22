a2355c386ad3f38182163b5b0f244ec7344acb7b
LucileDesfour <lucile.desfour@epitech.eu>
2021-12-22 12:00:35 +0100

    Update test_board.py

--- python/tests/test_board.py
+++ python/tests/test_board.py
@@ -29,3 +29,17 @@ def test_sould_get_8_mines_on_board():
                     count_mines += 1
     
     assert(count_mines == 8)
+
+# Test discover tile 
+
+def test_sould_explode_when_discovering_a_bomb():
+    board = Board(10, 20)
+    board.init_board(8)
+
+    for idx_row, row in enumerate(board._tiles):
+        for idx_tile, tile in enumerate(row):
+            if (tile.is_mined()):
+                value = board.get_value(idx_row, idx_tile)
+                pass
+    
+    assert(value == "BOOM")
